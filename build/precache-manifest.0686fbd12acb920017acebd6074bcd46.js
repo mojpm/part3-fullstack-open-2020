3aa9fa285804ae28066cac81aa292c770771fed2
mojpm <itsjoelpm@gmail.com>
2020-12-03 19:26:53 +0300

    add production build

---
+++ build/precache-manifest.0686fbd12acb920017acebd6074bcd46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e62f8c991df779ffe2995461e4f55e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "808291ac4fac99a4aaed",
+    "url": "/static/js/2.05fa691e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.05fa691e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d836135022e3e6424395",
+    "url": "/static/js/main.eafd47df.chunk.js"
+  },
+  {
+    "revision": "8239c111dcefba6123fa",
+    "url": "/static/js/runtime-main.b252ddaf.js"
+  }
+]);
